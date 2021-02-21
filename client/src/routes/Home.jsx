dc27f847c7b6ee198308d98b28c5df17d8199730
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-02-21 08:08:03 -0800

    initial client architecture

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import AddRestaurant from '../components/AddRestaurant';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <AddRestaurant />
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RestaurantDetailPage() {
+  return <div>RestaurantDetailPage</div>;
+}
+
+export default RestaurantDetailPage;
